a90ab574465c5dc2b986d11eee288e4efd262297
Guillermo Abriola <guabriola@gmail.com>
2024-05-23 19:52:01 -0300

    Se deja configurado hasta la conexión de la BDD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksService } from './books/books.service';
 import { BooksController } from './books/books.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+
+    //Lo cargo asi porque con la configuración de ormconfig.json no funciona
+    //Más adelante lo cambio por variables de entorno.
+    TypeOrmModule.forRoot({
+      "type": "mysql",
+      "host": "localhost",
+      "port": 3306,
+      "username": "root",
+      "password": "password",
+      "database": "reto2_library",
+      "entities": ["dist/**/*.entity{.ts,.js}"], 
+      "synchronize": true 
+  }),
+  ],
   controllers: [AppController, BooksController],
   providers: [AppService, BooksService],
 })
--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,18 +1,23 @@
 import { Controller, Get, Param, Post, Req, Body, Delete, Put } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { Request } from 'express';
+import { UpdateBookDto } from './UpdateBook.dto';
+import { BookDto } from './book.dto';
+import { Book } from './book.class';
+
 
 @Controller('books')
 export class BooksController {
     constructor(private booksService: BooksService){}
     
     @Get()
-    findAll(@Req() request: Request) { 
-      return this.booksService.findAll(request.query); 
+    findAll(@Req() request: Request) : Book[]{ 
+        console.log(request.query);
+        return this.booksService.findAll(request.query); 
     }
 
     @Get(':bookId')
-    findBook(@Param('bookId') bookId: string){
+    findBook(@Param('bookId') bookId: string) : Book{
         return this.booksService.findBook(bookId);
     }
 
@@ -23,19 +28,17 @@ export class BooksController {
     // }
 
     @Post()
-    createBook(@Body() body){
-        let newBook: any = body;
+    createBook(@Body() newBook: BookDto) : Book{
         return this.booksService.createBook(newBook);
     }
 
     @Delete(':bookId')
-    deleteBook(@Param('bookId') bookId: string){
+    deleteBook(@Param('bookId') bookId: string) : Book{
         return this.booksService.deleteBook(bookId);
     }
 
     @Put(':bookId')
-    updateBook(@Param('bookId') bookId: string, @Body() body){
-        let newBook: any = body;
+    updateBook(@Param('bookId') bookId: string, @Body() newBook: UpdateBookDto): Book{
         return this.booksService.updateBook(bookId, newBook);
     }
 }
