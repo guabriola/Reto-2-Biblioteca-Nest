b622e2bf9ff4d0650a7b1ad5e47dc2f118efd5ad
Guillermo Abriola <guabriola@gmail.com>
2024-05-29 20:31:47 -0300

    Role-based authorization configurated- based on the link https://shpota.com/2022/07/16/role-based-authorization-with-jwt-using-nestjs.html

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
-import { AuthGuard } from '@nestjs/passport';
+// import { AuthGuard } from '@nestjs/passport';
+import { HasRoles } from './auth/has-roles.decorator';
+import { Role } from './model/role.enum';
+import { RolesGuard } from './auth/roles.guard';
+
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
@@ -10,19 +16,34 @@ export class AppController {
     private authService: AuthService,
   ) {}
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
 
   //Endpoint to test the request with jwt
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
   }
 
+  //Endpoint to test the request with jwt for role ADMIN
+  @HasRoles(Role.Admin)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('admin')
+  onlyAdmin(@Request() req) {
+    return req.user;
+  }
+
+  //Endpoint to test the request with jwt for role USER
+  @HasRoles(Role.User)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('user')
+  onlyUser(@Request() req) {
+    return req.user;
+  }
   // @Get()
   // getHello(): string {
   //   return this.appService.getHello();
