72bea9aca1449ef8c596c7f16c7dbf786e31d1d9
Guillermo Abriola <guabriola@gmail.com>
2024-05-29 20:12:55 -0300

    JWT strategy implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,30 @@
 import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
 
   @UseGuards(AuthGuard('local'))
   @Post('auth/login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  //Endpoint to test the request with jwt
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,22 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
+
 
 @Module({
-    imports: [UsersModule, PassportModule],
-    providers: [AuthService, LocalStrategy],
+    imports: [
+        UsersModule, 
+        PassportModule,
+        JwtModule.register({
+            //Todo ----->>>>> Change secretJWT to a env Avriable its a must.
+            secret: jwtConstants.secret,
+            signOptions: { expiresIn: '1h' },
+          }),
+    ],
+    providers: [AuthService, LocalStrategy, JwtStrategy],
+    exports: [AuthService],
 })
 export class AuthModule {}
