9092c527373ae007c0d345d644aef2323a6b1191
Guillermo Abriola <guabriola@gmail.com>
2024-08-15 15:34:46 -0300

    Test Throttler Guard Error solved in user.controller test.

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,15 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      providers: [
+        {
+          provide: UsersService,
+          //Moking user service
+          useValue: {},
+        },
+      ],
+    })
+      //Override ThrottleGuard.
+      .overrideGuard(ThrottlerGuard) 
+      //Moking the response of the guard.
+      .useValue({ canActivate: jest.fn(() => true) })
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
